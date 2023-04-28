89a9b0391517955fcb87c7c9bdb93b5533d98258
112292454 <1154459434@qq.com>
2023-04-28 14:02:29 +0800

    set remote server nacos

--- gcx/dslab-event/src/test/java/com/dslab/event/DslabEventApplicationTests.java
+++ gcx/dslab-event/src/test/java/com/dslab/event/DslabEventApplicationTests.java
@@ -3,9 +3,10 @@ package com.dslab.event;
 import com.dslab.event.mapper.EventMapper;
 import com.dslab.event.utils.MathUtils;
 import com.dslab.event.utils.TimeUtils;
-import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class DslabEventApplicationTests {
