c9ffa700ccc851979aa23d438e2f5c7911c32d53
郭晨旭 <2269409349@qq.com>
2023-06-02 01:34:12 +0800

    更新 pom

--- gcx/dslab-event/src/main/java/com/dslab/event/controller/UserController.java
+++ gcx/dslab-event/src/main/java/com/dslab/event/controller/UserController.java
@@ -4,10 +4,7 @@ import com.dslab.commonapi.entity.User;
 import com.dslab.commonapi.services.UserService;
 import com.dslab.commonapi.vo.Result;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +17,7 @@ import java.util.List;
  **/
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 public class UserController {
     @Resource
