0ac2315e7ac160cdff154c68659c954befc6ccfc
112292454 <1154459434@qq.com>
2023-06-02 01:52:28 +0800

    1

--- gcx/dslab-event/src/main/java/com/dslab/event/controller/UserController.java
+++ gcx/dslab-event/src/main/java/com/dslab/event/controller/UserController.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserService userService;
-    
+
 
     /**
      * 根据邮箱获取用户信息
@@ -33,6 +33,7 @@ public class UserController {
      * @return 用户信息
      */
     @GetMapping("/mail/{mail}")
+    
     public Result<User> getByMail(@PathVariable String mail) {
         User user = userService.loadByMail(mail);
         if (user == null) {
